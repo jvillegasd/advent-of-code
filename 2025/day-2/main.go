bf37c6f806cb8df2ab61c3062d1a90cc20a9fed0
Jvillegasd <johnnyvillegaslrs@gmail.com>
2025-12-03 02:17:04 -0500

    feat: update day-2 solution with range parsing and invalid ID summation

--- 2025/day-2/main.go
+++ 2025/day-2/main.go
@@ -2,26 +2,54 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
 	"strings"
 )
 
-func parseInput(line string) (int, int) {
-	parts := strings.Split(line, "-")
-	start, err := strconv.Atoi(parts[0])
-	if err != nil {
-		log.Fatal(err)
-	}
-	end, err := strconv.Atoi(parts[1])
-	if err != nil {
-		log.Fatal(err)
-	}
+func parseRange(rangeStr string) (string, string) {
+	parts := strings.Split(rangeStr, "-")
+	start := parts[0]
+	end := parts[1]
 	return start, end
 }
 
+func findInvalidIDsSum(start, end string) int64 {
+	sum := int64(0)
+
+	startInt, _ := strconv.Atoi(start)
+	endInt, _ := strconv.Atoi(end)
+
+	var halfStart int
+	if len(start)/2 == 0 {
+		halfStart, _ = strconv.Atoi(start)
+	} else {
+		halfStart, _ = strconv.Atoi(start[:len(start)/2])
+	}
+
+	var halfEnd int
+	if (len(end)+1)/2 == 0 {
+		halfEnd, _ = strconv.Atoi(end)
+	} else {
+		halfEnd, _ = strconv.Atoi(end[:(len(end)+1)/2])
+	}
+
+	for i := halfStart; i <= halfEnd; i++ {
+		potentialID, _ := strconv.Atoi(fmt.Sprintf("%d%d", i, i))
+		if potentialID >= startInt && potentialID <= endInt {
+			sum += int64(potentialID)
+		}
+	}
+
+	return sum
+}
+
 func main() {
+	sum := int64(0)
+	var rangeStrings []string
+
 	file, err := os.Open("2025/day-2/input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -29,8 +57,19 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
+	if scanner.Scan() {
 		line := scanner.Text()
-		start, end := parseInput(line)
+		rangeStrings = strings.Split(line, ",")
 	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, rangeStr := range rangeStrings {
+		start, end := parseRange(strings.TrimSpace(rangeStr))
+		sum += findInvalidIDsSum(start, end)
+	}
+
+	fmt.Println("Part 1: ", sum)
 }
