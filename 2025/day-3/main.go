d13accd30acb150c60ed04d06f716a8fd2e3804b
Jvillegasd <johnnyvillegaslrs@gmail.com>
2025-12-03 23:47:29 -0500

    feat: generalize function

--- 2025/day-3/main.go
+++ 2025/day-3/main.go
@@ -16,33 +16,12 @@ func parseInput(line string) []int {
 	return batteryBanks
 }
 
-func getMaxJoltage(line string) int {
-	maxBatteryIdx := 0
-	batteryBanks := parseInput(line)
-	secondMaxBatteryIdx := len(batteryBanks) - 1
-
-	for i := 1; i < len(batteryBanks)-1; i++ {
-		if batteryBanks[i] > batteryBanks[maxBatteryIdx] {
-			maxBatteryIdx = i
-		}
-	}
-
-	for i := len(batteryBanks) - 2; i > maxBatteryIdx; i-- {
-		if batteryBanks[i] > batteryBanks[secondMaxBatteryIdx] {
-			secondMaxBatteryIdx = i
-		}
-	}
-
-	maxJoltage, _ := strconv.Atoi(fmt.Sprintf("%d%d", batteryBanks[maxBatteryIdx], batteryBanks[secondMaxBatteryIdx]))
-	return maxJoltage
-}
-
-func getMaxJoltage2(line string) int64 {
+func getMaxJoltage(line string, size int) int64 {
 	idx := 0
-	maxBatteryIdxs := make([]int, 12)
+	maxBatteryIdxs := make([]int, size)
 	batteryBanks := parseInput(line)
 
-	for i := 0; i < len(maxBatteryIdxs); i++ {
+	for i := 0; i < size; i++ {
 		maxIdx := idx
 		digitsLeft := len(maxBatteryIdxs) - 1 - i
 		for j := idx + 1; j < len(batteryBanks)-digitsLeft; j++ {
@@ -63,7 +42,7 @@ func getMaxJoltage2(line string) int64 {
 }
 
 func main() {
-	sum := 0
+	sum := int64(0)
 	sum2 := int64(0)
 
 	file, err := os.Open("2025/day-3/input.txt")
@@ -75,8 +54,8 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		sum += getMaxJoltage(line)
-		sum2 += getMaxJoltage2(line)
+		sum += getMaxJoltage(line, 2)
+		sum2 += getMaxJoltage(line, 12)
 	}
 
 	if err := scanner.Err(); err != nil {
