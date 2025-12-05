95e0934ed586edd34352b08f63d68d5ffe3ee0f5
Jvillegasd <johnnyvillegaslrs@gmail.com>
2025-12-05 02:25:09 -0500

    feat: implement merging of ranges and count contiguous IDs for day 5 solution

--- 2025/day-5/main.go
+++ 2025/day-5/main.go
@@ -15,8 +15,57 @@ type Range struct {
 	end   int64
 }
 
+func mergeRanges(ranges []Range) []Range {
+	if len(ranges) <= 1 {
+		return ranges
+	}
+
+	slices.SortFunc(ranges, func(a, b Range) int {
+		if a.start != b.start {
+			return int(a.start - b.start)
+		}
+		return int(a.end - b.end)
+	})
+
+	mergedRanges := []Range{ranges[0]}
+	for i := 1; i < len(ranges); i++ {
+		last := &mergedRanges[len(mergedRanges)-1]
+		current := ranges[i]
+
+		if current.start <= last.end+1 {
+			if current.end > last.end {
+				last.end = current.end
+			}
+		} else {
+			mergedRanges = append(mergedRanges, current)
+		}
+	}
+
+	return mergedRanges
+}
+
+func countFreshIds(ranges []Range, availableIds []int64) int64 {
+	count := int64(0)
+	for _, id := range availableIds {
+		for _, r := range ranges {
+			if id >= r.start && id <= r.end {
+				count++
+				break
+			}
+		}
+	}
+	return count
+}
+
+func countContiguousIds(ranges []Range) int64 {
+	count := int64(0)
+	for _, r := range ranges {
+		count += r.end - r.start + 1
+	}
+	return count
+}
+
 func main() {
-	countFreshIds := 0
 	ranges := []Range{}
 	availableIds := []int64{}
 	afterBlankLine := false
@@ -49,20 +98,10 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for _, r := range ranges {
-		idsToRemove := []int64{}
-		for _, id := range availableIds {
-			if id >= r.start && id <= r.end {
-				countFreshIds++
-				idsToRemove = append(idsToRemove, id)
-			}
-		}
-		for _, idToRemove := range idsToRemove {
-			availableIds = slices.DeleteFunc(availableIds, func(currentId int64) bool {
-				return currentId == idToRemove
-			})
-		}
-	}
+	mergedRanges := mergeRanges(ranges)
+	countFreshIds := countFreshIds(mergedRanges, availableIds)
+	countContiguousIds := countContiguousIds(mergedRanges)
 
 	fmt.Println("Part 1: ", countFreshIds)
+	fmt.Println("Part 2: ", countContiguousIds)
 }
