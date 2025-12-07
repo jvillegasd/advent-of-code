c7ddd5d1d6997df83d8a04a56b4f5db12a7f8b88
Jvillegasd <johnnyvillegaslrs@gmail.com>
2025-12-07 15:29:17 -0500

    refactor: restructure main function to integrate part 1 and part 2 solutions with improved counting logic for day 7 challenge

--- 2025/day-7/main.go
+++ 2025/day-7/main.go
@@ -7,38 +7,9 @@ import (
 	"os"
 )
 
-func solvePart1(diagram []string) int {
-	count := 0
-
-	lastBeams := make([]bool, len(diagram[0]))
-	for i := 0; i < len(diagram[0]); i++ {
-		if diagram[0][i] == 'S' {
-			lastBeams[i] = true
-		}
-	}
-
-	currentBeams := lastBeams
-	for i := 1; i < len(diagram); i++ {
-		for j := 0; j < len(diagram[i]); j++ {
-			if diagram[i][j] == '^' && lastBeams[j] {
-				currentBeams[j] = false
-				count++
-
-				if j+1 < len(diagram[i]) {
-					currentBeams[j+1] = true
-				}
-				if j-1 >= 0 {
-					currentBeams[j-1] = true
-				}
-			}
-		}
-		lastBeams = currentBeams
-	}
-
-	return count
-}
-
 func main() {
+	count1 := 0
+	count2 := 1
 	diagram := []string{}
 
 	file, err := os.Open("2025/day-7/input.txt")
@@ -57,7 +28,39 @@ func main() {
 		log.Fatal(err)
 	}
 
-	part1 := solvePart1(diagram)
+	lastBeams := make([]bool, len(diagram[0]))
+	lastBeansTimelines := make([]int, len(diagram[0]))
+	for i := 0; i < len(diagram[0]); i++ {
+		if diagram[0][i] == 'S' {
+			lastBeams[i] = true
+			lastBeansTimelines[i] = 1
+		}
+	}
 
-	fmt.Println("Part 1: ", part1)
+	currentBeams := lastBeams
+	currentBeansTimelines := lastBeansTimelines
+	for i := 1; i < len(diagram); i++ {
+		for j := 0; j < len(diagram[i]); j++ {
+			if diagram[i][j] == '^' && lastBeams[j] {
+				currentBeams[j] = false
+				count1++
+				count2 += currentBeansTimelines[j]
+
+				if j+1 < len(diagram[i]) {
+					currentBeams[j+1] = true
+					currentBeansTimelines[j+1] += currentBeansTimelines[j]
+				}
+				if j-1 >= 0 {
+					currentBeams[j-1] = true
+					currentBeansTimelines[j-1] += currentBeansTimelines[j]
+				}
+				currentBeansTimelines[j] = 0
+			}
+		}
+		lastBeams = currentBeams
+		lastBeansTimelines = currentBeansTimelines
+	}
+
+	fmt.Println("Part 1: ", count1)
+	fmt.Println("Part 2: ", count2)
 }
