30d1df8b2fe2c28c79117fabb97482463e7d2838
Jvillegasd <johnnyvillegaslrs@gmail.com>
2025-12-07 03:03:37 -0500

    refactor: replace applyOperation with strategy map for improved operation handling in day 6 solution

--- 2025/day-6/main.go
+++ 2025/day-6/main.go
@@ -9,21 +9,16 @@ import (
 	"strings"
 )
 
-func applyOperation(operation string, a []int) int64 {
-	if len(a) == 0 {
-		return int64(0)
-	}
-
-	switch operation {
-	case "*":
-		return int64(a[0]) * int64(a[1]) * int64(a[2]) * int64(a[3])
-	case "+":
-		return int64(a[0]) + int64(a[1]) + int64(a[2]) + int64(a[3])
-	}
-	return int64(0)
-}
-
 func solvePart1(operations [][]string) int64 {
+	strategies := map[string]func([]int) int64{
+		"*": func(a []int) int64 {
+			return int64(a[0]) * int64(a[1]) * int64(a[2]) * int64(a[3])
+		},
+		"+": func(a []int) int64 {
+			return int64(a[0]) + int64(a[1]) + int64(a[2]) + int64(a[3])
+		},
+	}
+
 	sum := int64(0)
 	for i := 0; i < len(operations[0]); i++ {
 		a, _ := strconv.Atoi(operations[0][i])
@@ -32,13 +27,19 @@ func solvePart1(operations [][]string) int64 {
 		d, _ := strconv.Atoi(operations[3][i])
 
 		numbers := []int{a, b, c, d}
-		sum += int64(applyOperation(operations[4][i], numbers))
+		sum += strategies[operations[4][i]](numbers)
 	}
+	return sum
+}
+
+func solvePart2(operations []string) int64 {
+	sum := int64(0)
 	return sum
 }
 
 func main() {
 	operationsPart1 := [][]string{}
+	operationsPart2 := []string{}
 
 	file, err := os.Open("2025/day-6/input.txt")
 	if err != nil {
@@ -51,6 +52,7 @@ func main() {
 		line := scanner.Text()
 		// Use Fields to split on whitespace and remove empty strings
 		operationsPart1 = append(operationsPart1, strings.Fields(line))
+		operationsPart2 = append(operationsPart2, line)
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -58,6 +60,8 @@ func main() {
 	}
 
 	part1 := solvePart1(operationsPart1)
+	part2 := solvePart2(operationsPart2)
 
 	fmt.Println("Part 1: ", part1)
+	fmt.Println("Part 2: ", part2)
 }
