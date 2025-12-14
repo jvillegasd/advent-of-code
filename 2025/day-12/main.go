489195ac97234b75474632c4247dfd623b4229e0
Jvillegasd <johnnyvillegaslrs@gmail.com>
2025-12-14 00:26:02 -0500

    feat: add initial implementation for input parsing and shape/region structures in day 12 challenge

---
+++ 2025/day-12/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Shape struct {
+	Index   int
+	Pattern [][]rune
+	Width   int
+	Height  int
+}
+
+type Region struct {
+	Width      int
+	Height     int
+	Quantities []int
+}
+
+type Input struct {
+	Shapes  []Shape
+	Regions []Region
+}
+
+func parseInput(filename string) *Input {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	input := &Input{
+		Shapes:  []Shape{},
+		Regions: []Region{},
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line == "" {
+			continue
+		}
+
+		if strings.Contains(line, ":") && !strings.Contains(line, "x") {
+			parts := strings.Split(line, ":")
+			index, _ := strconv.Atoi(parts[0])
+
+			pattern := [][]rune{}
+			for i := 0; i < 3; i++ {
+				if !scanner.Scan() {
+					log.Fatalf("unexpected end of file while reading shape %d", index)
+				}
+				pattern = append(pattern, []rune(scanner.Text()))
+			}
+
+			width := 0
+			if len(pattern) > 0 {
+				width = len(pattern[0])
+			}
+			height := len(pattern)
+
+			input.Shapes = append(input.Shapes, Shape{
+				Index:   index,
+				Pattern: pattern,
+				Width:   width,
+				Height:  height,
+			})
+		} else if strings.Contains(line, "x") {
+			parts := strings.Split(line, ": ")
+			dims := strings.Split(parts[0], "x")
+			width, _ := strconv.Atoi(dims[0])
+			height, _ := strconv.Atoi(dims[1])
+
+			quantityStrs := strings.Fields(parts[1])
+			quantities := make([]int, len(quantityStrs))
+			for i, qStr := range quantityStrs {
+				q, _ := strconv.Atoi(qStr)
+				quantities[i] = q
+			}
+
+			input.Regions = append(input.Regions, Region{
+				Width:      width,
+				Height:     height,
+				Quantities: quantities,
+			})
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return input
+}
+
+func main() {
+	input := parseInput("2025/day-12/input.txt")
+	fmt.Println(input)
+
+	fmt.Println("Part 1:", 0)
+	fmt.Println("Part 2:", 0)
+}
